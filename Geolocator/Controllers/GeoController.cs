ff9915b294d441d660fc9b70c6521b6d0cb15efa
Highyard <benjaminoest@hotmail.com>
2020-12-08 10:17:59 +0100

    first commit

---
+++ Geolocator/Controllers/GeoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Geolocator.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geolocator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoController : ControllerBase
+    {
+        // GET: api/Geo/
+        [HttpGet(Name = "Get")]
+        public ICollection<Restaurant> Get([FromHeader]ICollection<string> addresses)
+        {
+            var restaurants = new List<Restaurant>();
+            // HTTP GET
+            // loop over addresses
+            // GET request to fetch lat and long of address
+
+            return restaurants;
+        }
+
+        // POST: api/Geo
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Geo/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Geolocator/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geolocator.Models
+{
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+        public int PriceLevel { get; set; }
+    }
+}
